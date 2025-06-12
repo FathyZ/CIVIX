0561138e3dbf540c71b4806048f5fa6516d38cb7
FathyZ <98699157+FathyZ@users.noreply.github.com>
2025-06-12 19:46:52 +0300

    Sidebar is finally responsive

--- src/app/Components/home/sidebar/sidebar.component.ts
+++ src/app/Components/home/sidebar/sidebar.component.ts
@@ -5,10 +5,13 @@ import { Router, RouterModule } from '@angular/router';
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [RouterModule , NgClass],
+  imports: [RouterModule , NgClass , CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
-
+isOpen = true;
+toggleSidebar() {
+  this.isOpen = !this.isOpen;
+}
 }
